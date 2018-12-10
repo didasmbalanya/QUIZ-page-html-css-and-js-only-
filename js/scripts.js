741321dfc4974d95ececdf8a380ea9d176771d6a
Didas Mbalanya <didasmbalanya@gmail.com>
2018-12-10 12:00:05 +0300

    add function and syntax to scripts file

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+answers = ["Abraham Lincoln","true","China","Tesla"]
+$(document).ready(function(){
+	var total = 0;
+	var a1 = $("input[type=radio][name=answer1:checked").val();
+	var a2 = $("input[type=radio][name=answer2:checked").val();
+	var a3 = $("input[type=radio][name=answer3:checked").val();
+	var a4 = $("input[type=radio][name=answer4:checked").val();
+	
+	
+	if (a1 === undefined || a2 === undefined || a3 === undefined || a4 === undefined){
+		alert ("Complete all the questions")
+	}
+	else{
+		if(a1 === "")
+	}
+})
